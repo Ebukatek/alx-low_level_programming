69286db2bbbd566fe3493972033f39f733e21d9d
Ebukatek <olisachukwuebuka1@gmail.com>
2023-08-10 16:36:25 -0700

    pissed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -this is the entry point
+ *
+ * Return: always return 0 when succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
