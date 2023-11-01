a610ca6cc5cf00fe230a9c09ea8bb57992414699
Ebukatek <olisachukwuebuka1@gmail.com>
2023-11-01 13:03:39 -0700

    took

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: counts the argument
+ * @argv: the argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
