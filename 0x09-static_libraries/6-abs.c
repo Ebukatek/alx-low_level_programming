c798ac66d2297f4ca31fb087a2aab820d1ede1db
Ebukatek <olisachukwuebuka1@gmail.com>
2023-10-31 19:27:42 -0700

    pose

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -3,15 +3,18 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @c: The number to be computed
- * Return: -c or c;
+ * Return: Absolute value of number or zero
  */
 
 int _abs(int c)
 {
 	if (c < 0)
-		return (-c);
-	else if (c >= 0)
 	{
-		return (c);
+		int abs_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
 	}
+		return (c);
 }
