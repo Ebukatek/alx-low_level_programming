123849951a3414e3c70a37bfa571a16337db4484
Ebukatek <olisachukwuebuka1@gmail.com>
2023-11-07 15:15:13 -0800

    wisdom

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dogs structure
+ * @name: dogs name
+ * @age: age of dog
+ * @owner: owners name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
